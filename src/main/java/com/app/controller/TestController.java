378cffee0d6f0a404bbe1cfeea958e99f968152a
JJAJJAKIM <csm9121@gmail.com>
2024-08-21 10:54:20 +0900

    jwt 토큰 생성 예제 수정 및 토큰 유효시간 검증 완료

--- src/main/java/com/app/controller/TestController.java
+++ src/main/java/com/app/controller/TestController.java
@@ -1,8 +1,10 @@
 package com.app.controller;
 
 import com.app.util.Token;
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +21,13 @@ public class TestController {
     @GetMapping("/token")
     public String token() {
         return token.setToken();
+    }
+
+    @GetMapping("/token/{token}")
+    public Claims token(@PathVariable("token") String jwt) {
+       if(token.isValidToken(jwt)){
+           return token.getToken(jwt);
+       }
+       return null;
     }
 }
--- src/main/java/com/app/util/Token.java
+++ src/main/java/com/app/util/Token.java
@@ -1,60 +1,119 @@
 package com.app.util;
 
-import io.jsonwebtoken.Jwt;
-import io.jsonwebtoken.JwtBuilder;
-import io.jsonwebtoken.Jwts;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-
-import javax.crypto.SecretKey;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.crypto.SecretKey;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.MacAlgorithm;
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @Component
 public class Token {
 
-    public String setToken() {
+	private final MacAlgorithm ALGORITHM = Jwts.SIG.HS256;
 
-        JwtBuilder token = Jwts.builder()
-                .header().add(getHeader()).and()
-                .claims(setClaims())
-                .signWith(getSecretKey())
-        ;
-        log.info("Token : {}",token.compact());
-        return token.compact();
-    }
+	@Value("${access.auth.jwt}")
+	private String jwtSecretKey;
 
-    private Map<String, String> getHeader() {
-        Map<String, String> header = new HashMap<>();
-        header.put("alg", "HS256");
-        header.put("typ", "JWT");
-        return header;
-    }
+	@Value("${access.auth.interval}")
+	private int interval;
+	
+	public String setToken() {
+		
+		JwtBuilder token = Jwts.builder()
+				.header().add(getHeader()).and()
+				.claims(setClaims())
+				.expiration(getDate())
+				.issuedAt(Calendar.getInstance().getTime())
+			.signWith(getKey(), ALGORITHM);
+		
+		return token.compact();
+	}
+	
+	private Map<String, String> getHeader() {
+		Map<String, String> header = new HashMap<>();
+		header.put("alg", "HS256");
+		header.put("typ", "JWT");
+		return header;
+	}
+	
+	private SecretKey getKey() {
+//		return Jwts.SIG.HS256.key().build();
+	    return Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(jwtSecretKey));
+//		return Keys.hmacShaKeyFor(jwtSecretKey.getBytes());
+	}
+	
+	/********************************************************************
+	 * iss : 토큰 발급자	(issuer)
+	 * sub : 토큰 제목		(subject)
+	 * aud : 토큰 대상자	(audience)
+	 * exp : 토큰 만료 시간	(expiration)
+	 * iat : 토큰 발급 시간	(issuedAt)
+	 * nbf : 토큰 활성 날짜	(not before)
+	 ********************************************************************/
+	private Map<String, ?> setClaims() {
+		Map<String, Object> claims = new HashMap<>();
 
-    private SecretKey getSecretKey() {
-        return Jwts.SIG.HS256.key().build();
-    }
+		Map<String, Object> user = new HashMap<>();
+		user.put("userNm", "사용자");
+		
+		claims.put("issuer", "JsonAPI");
+		claims.put("subject", "User");
+		claims.put("audience", user);
+//		claims.put("expiration", getDate());
+//		claims.put("issuedAt", Calendar.getInstance().getTime());
+				
+		return claims;
+	}
+	
+	private Date getDate() {
+		Calendar date = Calendar.getInstance();
+		date.add(Calendar.MINUTE, interval);
+		return date.getTime();
+	}
+	
+	public Claims getToken(String token) {
+		Claims claims = Jwts.parser()
+				.verifyWith(getKey())
+				.build()
+				.parseSignedClaims(token)
+				.getPayload();
+		log.info("Claims : {}", claims.get("audience"));
+		return claims;
+	}
 
-    private Map<String, ?> setClaims() {
-        Map<String, Object> claims = new HashMap<>();
-
-        Map<String, Object> user = new HashMap<>();
-        user.put("userNm", "사용자");
-
-        claims.put("issuer", "JsonAPI");
-        claims.put("subject", "User");
-        claims.put("audience", user);
-        claims.put("iat", Calendar.getInstance().getTime().getTime());
-        claims.put("expiration", getDate(1));
-        return claims;
-    }
-
-    private Date getDate(int i){
-        Calendar date = Calendar.getInstance();
-        date.add(Calendar.MINUTE, i);
-        return date.getTime();
-    }
+	public boolean isValidToken(String token) {
+		try {
+			if(token != null || !token.isEmpty()) {
+				Claims claims = getToken(token);
+				log.info("============================================");
+				log.info("|ExpireTime\t: {}|", claims.getExpiration());
+				log.info("|IIssuedTime: {}|", claims.getIssuedAt());
+				log.info("|RealTime\t: {}|", Calendar.getInstance().getTime());
+				log.info("============================================");
+				return true;
+			}
+		} catch (ExpiredJwtException exception) {
+			log.info("==============");
+			log.error("Token Expired");
+		} catch (JwtException exception) {
+			log.info("==============");
+			log.error("Token Tampered");
+		} catch (NullPointerException exception) {
+			log.info("==============");
+			log.error("Token is null");
+		}
+		log.info("==============");
+		return false;
+	}
 }
